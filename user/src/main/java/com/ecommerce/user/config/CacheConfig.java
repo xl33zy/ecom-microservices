428d3dfa816931489d6f57e9067d8781748fb549
Baubek Zhumabayev <baubekzhumabaev@gmail.com>
2025-12-20 21:55:56 +0500

    removing warnings: added CacheConfig to some services. divided gateway to profiles for correct zipkin work, and so on

---
+++ user/src/main/java/com/ecommerce/user/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package com.ecommerce.user.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(
+                com.github.benmanes.caffeine.cache.Caffeine.newBuilder()
+                                                           .maximumSize(1000)
+                                                           .expireAfterWrite(java.time.Duration.ofMinutes(10))
+        );
+        return cacheManager;
+    }
+}
